7dc790ac50a9d8b9771a0dfee736b456fd8eb15d
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-12-09 09:01:27 +0100

    Sidebar tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 import { TuiNavigation } from '@taiga-ui/layout';
 import { HeaderComponent } from './navigation/components/header/header.component';
 import { SidebarComponent } from './navigation/components/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [TuiRoot, TuiNavigation, HeaderComponent, SidebarComponent],
+  imports: [
+    TuiRoot,
+    TuiNavigation,
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/navigation/components/sidebar/sidebar.component.ts
+++ src/app/navigation/components/sidebar/sidebar.component.ts
@@ -2,53 +2,15 @@ import { Component } from '@angular/core';
 import { TuiAsideItemDirective, TuiNavigation } from '@taiga-ui/layout';
 import { BreakpointService } from '@app/common/services/breakpoint.service';
 import { SidebarService } from '@app/navigation/services/sidebar.service';
-
-interface sidebarItem {
-  label: string;
-  icon: string;
-}
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [TuiNavigation, TuiAsideItemDirective],
+  imports: [TuiNavigation, TuiAsideItemDirective, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-  public readonly headerItems: readonly sidebarItem[] = [
-    {
-      label: 'Home',
-      icon: '@tui.home',
-    },
-    {
-      label: 'Explore controversial',
-      icon: '@tui.flame',
-    },
-    {
-      label: 'Explore popular',
-      icon: '@tui.trophy',
-    },
-    {
-      label: 'Explore unpopular',
-      icon: '@tui.frown',
-    },
-  ];
-
-  public readonly middleItems: readonly sidebarItem[] = [
-    {
-      label: 'Post an idea',
-      icon: '@tui.plus',
-    },
-    {
-      label: 'Settings',
-      icon: '@tui.settings',
-    },
-    {
-      label: 'More',
-      icon: '@tui.ellipsis',
-    },
-  ];
-
   public constructor(
     public readonly breakpointsService: BreakpointService,
     public readonly sidebarService: SidebarService
