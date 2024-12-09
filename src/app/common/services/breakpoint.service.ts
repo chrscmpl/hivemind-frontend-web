18de8e6eead227e8bc4e6591d61b7efae0bc738d
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-12-09 01:10:42 +0100

    Add some items to the sidebar

--- src/app/common/services/breakpoint.service.ts
+++ src/app/common/services/breakpoint.service.ts
@@ -1,28 +1,61 @@
-import { Injectable } from '@angular/core';
+import {
+  computed,
+  Injectable,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { TuiBreakpointService } from '@taiga-ui/core';
-import { distinctUntilChanged, map, merge, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BreakpointService {
-  public readonly isMobile$: Observable<boolean>;
-  public readonly isDesktopSmall$: Observable<boolean>;
-  public readonly isDesktopLarge$: Observable<boolean>;
-  public readonly isDesktop$: Observable<boolean>;
+  private readonly _isMobile: WritableSignal<boolean> = signal(false);
+  private readonly _isDesktopSmall: WritableSignal<boolean> = signal(false);
+  private readonly _isDesktopLarge: WritableSignal<boolean> = signal(false);
+  private readonly _isDesktop: Signal<boolean> = computed(
+    () => this._isDesktopSmall() || this._isDesktopLarge()
+  );
+
+  public get isMobile(): Signal<boolean> {
+    return this._isMobile;
+  }
+
+  public get isDesktopSmall(): Signal<boolean> {
+    return this._isDesktopSmall;
+  }
+
+  public get isDesktopLarge(): Signal<boolean> {
+    return this._isDesktopLarge;
+  }
+
+  public get isDesktop(): Signal<boolean> {
+    return this._isDesktop;
+  }
 
   constructor(private readonly breakpointService: TuiBreakpointService) {
-    this.isMobile$ = this.breakpointService.pipe(
-      map((breakpoint) => breakpoint === 'mobile')
-    );
-    this.isDesktopSmall$ = this.breakpointService.pipe(
-      map((breakpoint) => breakpoint === 'desktopSmall')
-    );
-    this.isDesktopLarge$ = this.breakpointService.pipe(
-      map((breakpoint) => breakpoint === 'desktopLarge')
-    );
-    this.isDesktop$ = merge(this.isDesktopSmall$, this.isDesktopLarge$).pipe(
-      distinctUntilChanged()
-    );
+    this.breakpointService.subscribe((breakpoint) => {
+      switch (breakpoint) {
+        case 'mobile': {
+          this._isMobile.set(true);
+          this._isDesktopSmall.set(false);
+          this._isDesktopLarge.set(false);
+          break;
+        }
+        case 'desktopSmall': {
+          this._isMobile.set(false);
+          this._isDesktopSmall.set(true);
+          this._isDesktopLarge.set(false);
+          break;
+        }
+        case 'desktopLarge': {
+          this._isMobile.set(false);
+          this._isDesktopSmall.set(false);
+          this._isDesktopLarge.set(true);
+          break;
+        }
+      }
+    });
   }
 }
--- src/app/navigation/components/sidebar/sidebar.component.ts
+++ src/app/navigation/components/sidebar/sidebar.component.ts
@@ -1,16 +1,54 @@
 import { Component } from '@angular/core';
-import { TuiNavigation } from '@taiga-ui/layout';
-import { AsyncPipe } from '@angular/common';
+import { TuiAsideItemDirective, TuiNavigation } from '@taiga-ui/layout';
 import { BreakpointService } from '@app/common/services/breakpoint.service';
 import { SidebarService } from '@app/navigation/services/sidebar.service';
 
+interface sidebarItem {
+  label: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-sidebar',
-  imports: [TuiNavigation, AsyncPipe],
+  imports: [TuiNavigation, TuiAsideItemDirective],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  public readonly headerItems: readonly sidebarItem[] = [
+    {
+      label: 'Home',
+      icon: '@tui.home',
+    },
+    {
+      label: 'Explore controversial',
+      icon: '@tui.flame',
+    },
+    {
+      label: 'Explore popular',
+      icon: '@tui.trophy',
+    },
+    {
+      label: 'Explore unpopular',
+      icon: '@tui.frown',
+    },
+  ];
+
+  public readonly middleItems: readonly sidebarItem[] = [
+    {
+      label: 'Post an idea',
+      icon: '@tui.plus',
+    },
+    {
+      label: 'Settings',
+      icon: '@tui.settings',
+    },
+    {
+      label: 'More',
+      icon: '@tui.ellipsis',
+    },
+  ];
+
   public constructor(
     public readonly breakpointsService: BreakpointService,
     public readonly sidebarService: SidebarService
