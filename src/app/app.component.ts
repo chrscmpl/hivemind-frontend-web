ac19aff6be1b416e57adb0f00605cab8d7d780e0
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2024-12-09 23:38:53 +0100

    Add TuiScrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { TuiRoot } from '@taiga-ui/core';
+import { TuiRoot, TuiScrollbar } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { TuiNavigation } from '@taiga-ui/layout';
 import { HeaderComponent } from './navigation/components/header/header.component';
 import { SidebarComponent } from './navigation/components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { BreakpointService } from './common/services/breakpoint.service';
 
 @Component({
   selector: 'app-root',
   imports: [
     TuiRoot,
     TuiNavigation,
+    TuiScrollbar,
     RouterOutlet,
     HeaderComponent,
     SidebarComponent,
@@ -17,4 +19,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  public constructor(public readonly breakpoints: BreakpointService) {}
+}
