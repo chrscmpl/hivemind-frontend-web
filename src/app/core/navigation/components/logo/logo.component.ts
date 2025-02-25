5cf32df16665658e7ac86e8b76de3747099f6bd8
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2025-02-25 12:13:17 +0100

    Add custom logo to terminal theme

--- src/app/core/navigation/components/logo/logo.component.ts
+++ src/app/core/navigation/components/logo/logo.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeService } from '@app/core/misc/services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-logo',
@@ -7,14 +9,31 @@ import { RouterLink } from '@angular/router';
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss',
 })
-export class LogoComponent {
-  private _minimal: boolean = false;
-  @Input() public styleClass: string = '';
-  @Input() public set minimal(value: boolean | '') {
-    this._minimal = !!value || value === '';
+export class LogoComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+
+  private _src = 'logo.svg';
+
+  public get src(): string {
+    return this._src;
   }
 
-  public get minimal(): boolean {
-    return this._minimal;
+  @Input() public styleClass: string = '';
+
+  public constructor(private readonly themeService: ThemeService) {}
+
+  public ngOnInit(): void {
+    this.subscriptions.push(
+      this.themeService.themeStatus$.subscribe((status) => {
+        this._src =
+          status.theme === 'dark' && status.variations.dark === 'terminal'
+            ? 'logo-terminal.svg'
+            : 'logo.svg';
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
