dddb128083109d2bc2b8dac48d6f58457e135947
chrscmpl <chcampolongo1@gmail.com>
2025-02-26 08:29:33 +0100

    Replace humanize-duration with pretty-ms (esm). Minor fixes

--- src/app/shared/pipes/humanize-duration.pipe.ts
+++ src/app/shared/pipes/humanize-duration.pipe.ts
@@ -1,38 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import humanizeDuration from 'humanize-duration';
+import prettyMilliseconds from 'pretty-ms';
 
 @Pipe({
   name: 'humanizeDuration',
 })
 export class HumanizeDurationPipe implements PipeTransform {
-  private humanizer = humanizeDuration.humanizer({
-    languages: {
-      shortEn: {
-        y: () => 'y',
-        mo: () => 'mo',
-        w: () => 'w',
-        d: () => 'd',
-        h: () => 'h',
-        m: () => 'm',
-        s: () => 's',
-        ms: () => 'ms',
-      },
-    },
-    round: true,
-  });
-
-  public transform(
-    milliseconds: number,
-    format?: 'full' | 'short',
-    largest?: number,
-    spacer?: string,
-  ): string {
-    const duration = this.humanizer(milliseconds, {
-      largest: largest ?? Infinity,
-      language: format === 'short' ? 'shortEn' : 'en',
-      spacer: spacer ?? ' ',
+  public transform(milliseconds: number, compact?: boolean): string {
+    return prettyMilliseconds(milliseconds, {
+      compact,
     });
-
-    return duration;
   }
 }
