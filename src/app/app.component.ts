326d15207091fc7dff9253c843535589917ec02e
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2025-03-02 15:17:45 +0100

    Display navigation errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { TuiRoot, TuiScrollbar } from '@taiga-ui/core';
-import { Component, Inject } from '@angular/core';
+import { TuiAlertService, TuiRoot, TuiScrollbar } from '@taiga-ui/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './core/navigation/components/header/header.component';
 import { SidebarComponent } from './core/navigation/components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
@@ -8,6 +8,8 @@ import { ThemeService } from './core/misc/services/theme.service';
 import { CreateButtonComponent } from './core/navigation/components/create-button/create-button.component';
 import { AppBarComponent } from './core/navigation/components/app-bar/app-bar.component';
 import { AsyncPipe, DOCUMENT } from '@angular/common';
+import { NavigationUtilsService } from './core/misc/services/navigation-utils.service';
+import { take } from 'rxjs';
 @Component({
   selector: 'app-root',
   imports: [
@@ -23,10 +25,30 @@ import { AsyncPipe, DOCUMENT } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public constructor(
     @Inject(DOCUMENT) public readonly document: Document,
     public readonly breakpoints: BreakpointService,
     public readonly themeService: ThemeService,
+    private readonly navigationUtils: NavigationUtilsService,
+    private readonly alerts: TuiAlertService,
   ) {}
+
+  public ngOnInit(): void {
+    this.navigationUtils.navigationError$.subscribe((error) => {
+      if (error.error.message) {
+        this.displayError(error.error.message);
+      }
+    });
+  }
+
+  private displayError(message: string): void {
+    this.alerts
+      .open(message, {
+        appearance: 'negative',
+        label: 'Error',
+      })
+      .pipe(take(1))
+      .subscribe();
+  }
 }
--- src/app/core/misc/services/navigation-utils.service.ts
+++ src/app/core/misc/services/navigation-utils.service.ts
@@ -6,7 +6,7 @@ import {
   NavigationError,
   Router,
 } from '@angular/router';
-import { filter, map, take } from 'rxjs';
+import { filter, Observable, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,10 +14,35 @@ import { filter, map, take } from 'rxjs';
 export class NavigationUtilsService {
   private _backAction: ((defaultFn: () => void) => void) | null = null;
 
+  public readonly navigationStopped$: Observable<
+    NavigationEnd | NavigationCancel | NavigationError
+  >;
+  public readonly navigationEnd$: Observable<NavigationEnd>;
+  public readonly navigationCancel$: Observable<NavigationCancel>;
+  public readonly navigationError$: Observable<NavigationError>;
+
   constructor(
     private readonly router: Router,
     private readonly location: Location,
-  ) {}
+  ) {
+    this.navigationStopped$ = this.router.events.pipe(
+      filter(
+        (e) =>
+          e instanceof NavigationCancel ||
+          e instanceof NavigationError ||
+          e instanceof NavigationEnd,
+      ),
+    );
+    this.navigationEnd$ = this.router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+    );
+    this.navigationCancel$ = this.router.events.pipe(
+      filter((e) => e instanceof NavigationCancel),
+    );
+    this.navigationError$ = this.router.events.pipe(
+      filter((e) => e instanceof NavigationError),
+    );
+  }
 
   public set backAction(action: ((defaultFn: () => void) => void) | null) {
     this._backAction = action;
@@ -33,27 +58,14 @@ export class NavigationUtilsService {
   }
 
   public executeIfNavigationSuccess(fn: () => void): void {
-    this.isCurrentNavigationSuccessful$().subscribe((success) => {
+    this.navigationStopped$.pipe(take(1)).subscribe((success) => {
       if (success) fn();
     });
   }
 
   public executeIfNavigationFailure(fn: () => void): void {
-    this.isCurrentNavigationSuccessful$().subscribe((success) => {
+    this.navigationStopped$.pipe(take(1)).subscribe((success) => {
       if (!success) fn();
     });
-  }
-
-  private isCurrentNavigationSuccessful$() {
-    return this.router.events.pipe(
-      filter(
-        (e) =>
-          e instanceof NavigationCancel ||
-          e instanceof NavigationError ||
-          e instanceof NavigationEnd,
-      ),
-      take(1),
-      map((e) => e instanceof NavigationEnd),
-    );
   }
 }
