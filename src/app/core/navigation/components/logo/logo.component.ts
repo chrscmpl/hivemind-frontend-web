2530ae7081b2437d08e3c7c1b8f7a43990c892de
chrscmpl <96392458+chrscmpl@users.noreply.github.com>
2025-03-07 09:58:00 +0100

    Fixes to logo

--- src/app/core/navigation/components/logo/logo.component.ts
+++ src/app/core/navigation/components/logo/logo.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeService } from '@app/core/misc/services/theme.service';
+import { fromEvent, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-logo',
@@ -7,14 +9,63 @@ import { RouterLink } from '@angular/router';
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.scss',
 })
-export class LogoComponent {
-  private _styleClass = '';
+export class LogoComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  private _defaultClasses = 'w-full h-full app-logo cursor-pointer';
+
+  private _styleClass = this._defaultClasses;
 
   @Input() public set styleClass(value: string) {
-    this._styleClass = value + ' cursor-pointer';
+    this._styleClass = `${this._defaultClasses} ${value}`;
   }
 
   public get styleClass(): string {
     return this._styleClass;
+  }
+
+  constructor(
+    private themeService: ThemeService,
+    private element: ElementRef,
+  ) {}
+
+  public ngOnInit(): void {
+    this.subscriptions.push(
+      this.themeService.themeLoading$.subscribe((loading) => {
+        if (loading === true) {
+          this.removeBackground();
+        } else {
+          this.addBackground();
+        }
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  private removeBackground(): void {
+    this._defaultClasses = this._defaultClasses.replace('bg-logo', '');
+    this._styleClass = this._styleClass.replace('bg-logo', '');
+  }
+
+  private addBackground(): void {
+    const url = getComputedStyle(this.element.nativeElement)
+      .getPropertyValue('--logo-url')
+      .replace(/url\((.*)\)/, '$1');
+
+    if (!url) {
+      return;
+    }
+
+    const img = new Image();
+    fromEvent(img, 'load', { passive: true })
+      .pipe(take(1))
+      .subscribe(() => {
+        this._defaultClasses += ' bg-logo';
+        this._styleClass += ' bg-logo';
+      });
+
+    img.src = url;
   }
 }
